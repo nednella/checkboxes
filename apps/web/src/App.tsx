d74b92928caa3e381172ab679cc9db4f3d86141e
nednella <119449811+nednella@users.noreply.github.com>
2026-06-21 15:25:38 +0100

    feat: prettier

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,19 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
       <section id="center">
         <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+          <img
+            src={heroImg}
+            className="base"
+            width="170"
+            height="179"
+            alt=""
+          />
+          <img
+            src={reactLogo}
+            className="framework"
+            alt="React logo"
+          />
+          <img
+            src={viteLogo}
+            className="vite"
+            alt="Vite logo"
+          />
         </div>
         <div>
           <h1>Get started</h1>
@@ -34,35 +50,60 @@ function App() {
 
       <section id="next-steps">
         <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
+          <svg
+            className="icon"
+            role="presentation"
+            aria-hidden="true"
+          >
             <use href="/icons.svg#documentation-icon"></use>
           </svg>
           <h2>Documentation</h2>
           <p>Your questions, answered</p>
           <ul>
             <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
+              <a
+                href="https://vite.dev/"
+                target="_blank"
+              >
+                <img
+                  className="logo"
+                  src={viteLogo}
+                  alt=""
+                />
                 Explore Vite
               </a>
             </li>
             <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
+              <a
+                href="https://react.dev/"
+                target="_blank"
+              >
+                <img
+                  className="button-icon"
+                  src={reactLogo}
+                  alt=""
+                />
                 Learn more
               </a>
             </li>
           </ul>
         </div>
         <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
+          <svg
+            className="icon"
+            role="presentation"
+            aria-hidden="true"
+          >
             <use href="/icons.svg#social-icon"></use>
           </svg>
           <h2>Connect with us</h2>
           <p>Join the Vite community</p>
           <ul>
             <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
+              <a
+                href="https://github.com/vitejs/vite"
+                target="_blank"
+              >
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -74,7 +115,10 @@ function App() {
               </a>
             </li>
             <li>
-              <a href="https://chat.vite.dev/" target="_blank">
+              <a
+                href="https://chat.vite.dev/"
+                target="_blank"
+              >
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -86,7 +130,10 @@ function App() {
               </a>
             </li>
             <li>
-              <a href="https://x.com/vite_js" target="_blank">
+              <a
+                href="https://x.com/vite_js"
+                target="_blank"
+              >
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -98,7 +145,10 @@ function App() {
               </a>
             </li>
             <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+              <a
+                href="https://bsky.app/profile/vite.dev"
+                target="_blank"
+              >
                 <svg
                   className="button-icon"
                   role="presentation"
@@ -116,7 +166,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
